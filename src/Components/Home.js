f41a4a12cf094c6836d99d8aa4ddc10c4a32892c
Gerald Dizon <gdizon@xtendly.com>
2018-12-13 16:31:09 +0800

    fixingUI

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -4,118 +4,119 @@ import {Grid, Col, Row} from 'react-styled-flexboxgrid';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography'
-import '../Css/Home.css';
-import '../Css/Nav.css';
 import logo from '../Images/GuamPowerLogo.png';
+import '../Css/Home.css';
 
 
 class Home extends Component {
   render() {
     return (
         <React.Fragment>
-        <div style={{flexGrow: 1}}>
-            <AppBar position="static" style={{backgroundColor:"white"}}>
-            <Toolbar>
-                <Typography variant="h6" color="inherit" style={{flexGrow: 1}}>
-                <ul>
-                    <Link to ="/">
-                        <img class = "logo" src = {logo}></img>
-                    </Link>   
-                </ul>
-                </Typography>
-                <ul>
-                    <Link to ="/">
-                    </Link>
-                    <Link to ="/Contact">
-                    
-                    </Link>
-                    <Link to ="/AboutUs">
-                        <li>HOME</li>
-                    </Link>
-                    <Link to ="/Profile">
-                        <li>WHAT WE DO</li>
-                    </Link>
-                    <Link to ="/Profile">
-                        <li>LOGIN</li>
-                    </Link>
-                    <Link to ="/Profile">
-                        <li class="fas fa-search"></li>
-                    </Link>
-                </ul>
-             </Toolbar>
-            </AppBar>
-        </div>
-        <Row>
-            <Col xs={12} sm={12} md={12} lg={12}>
-                <div class = "bgMain">
-                    <Row center="lg" center = "xs">
-                        <div class = " ">
-                            <p class = "mainTitle" >LOREM IPSUM</p>
-                            <hr/>
-                            <p class = "sideTitle" >BRINGING ENERGY SOLUTIONS TO YOU</p>
-                        </div>
-                    </Row>
-                </div>
-            </Col>
-        </Row>
-        <Row>
-            <Col lg={4} xs={12}>
-                <p>Gerald</p>
-            </Col>
-            <Col lg={4} xs={12}>
-                <p>Gerald</p>
-            </Col>
-            <Col lg={4} xs={12}>
-                <p>Gerald</p>
-            </Col>
-        </Row>
-
-        </React.Fragment>
-        
-        /*<React.Fragment>
-            <Grid>
+            <Col>
                 <Row>
-                    <Col sm={12} xs ={12} lg={false} md={false} style={{textAlign: 'center'}}>
-                        <ul>
-                            <Link to ="/">
-                                <img class = "logo" src = {logo}></img>
-                            </Link>   
-                        </ul>
+                    <AppBar  style={{backgroundColor:"white"}}>
+                        <Toolbar>
+                        <Typography variant="h6" color="inherit" style={{flexGrow: 1}}>
+                            <Link to ="/"></Link>
+                            <img className = "logo" src = {logo}></img>
+                        </Typography>
+                        <Link to ="/">
+                        </Link>
+                        <Link to ="/Contact">
+                        </Link>
+                        <Link to ="/AboutUs">
+                            <li>HOME</li>
+                        </Link>
+                        <Link to ="/Profile">
+                            <li>WHAT WE DO</li>
+                        </Link>
+                        <Link to ="/Profile">
+                            <li>LOGIN</li>
+                        </Link>
+                        <Link to ="/Profile">
+                            <li className="fas fa-search"></li>
+                        </Link>     
+                        </Toolbar>
+                    </AppBar>
+                </Row>
+            </Col>
+            <Col> 
+                <Row>
+                <div style={{
+                        backgroundImage: 'url(https://images.unsplash.com/photo-1531366936337-7c912a4589a7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80), linear-gradient(to bottom, transparent, rgba(0, 0, 0, 0.54))',
+                        backgroundSize: 'cover, cover',
+                        backgroundPosition: 'center',
+                        backgroundBlendMode: 'multiply',
+                        width: '100%',
+                        textAlign: 'center',
+                        paddingTop: 100,
+                        paddingBottom: 100,
+                        height: 200
+                    }}>
+                        <p className="mainTitle">LOREM IPSUM</p>
+                        <hr style={{width:'50%'}}/>
+                        <p className="sideTitle">LOREM IPSUM</p>
+                    </div>
+                </Row>
+            </Col>
+        <Col>
+            <Grid fluid>
+                <Row>
+                    <Col style={{paddingTop:10, paddingBottom:10}} xs={12} sm={12} md={4} lg={4}>
+                        <p style={{textAlign:"center", }}>lorem ipsum</p>
                     </Col>
-                    <Col sm={false} xs={false} lg={2} md = {4}>
-                        <ul>
-                            <Link to ="/">
-                                <img class = "logo" src = {logo}></img>
-                            </Link>   
-                        </ul>
+                    <Col xs={12} sm={12} md={4} lg={4} style={{paddingTop:10, paddingBottom:10}}>
+                        <p style={{textAlign:"center"}}>lorem ipsum</p>
                     </Col>
-                    <Col lg={3} xs = {4} md = {4}>
-                    </Col>
-                    <Col lg={7} xs = {4} md = {4}>
-                            <Link to ="/">
-                            </Link>
-                            <Link to ="/Contact">
-                                <li>WHO WE ARE</li>
-                            </Link>
-                            <Link to ="/AboutUs">
-                                <li>OUR BRANDS</li>
-                            </Link>
-                            <Link to ="/Profile">
-                                <li>OUR COMMITMENT</li>
-                            </Link>
-                            <Link to ="/Profile">
-                                <li>CAREER</li>
-                            </Link>
-                            <Link to ="/Profile">
-                                <li>INVESTORS</li>
-                            </Link>
-                            <Link to ="/Profile">
-                            </Link>
+                    <Col xs={12} sm={12} md={4} lg={4} style={{paddingTop:10, paddingBottom:10}}>
+                        <p style={{textAlign:"center"}}>lorem ipsum</p>
                     </Col>
                 </Row>
             </Grid>
-                
-            </React.Fragment>*/
+        </Col>
+        <Col>
+            <Grid>
+                <Row>
+                    <Col xs={12} sm={12} md={6} lg={6} >
+                        <div style={{
+                            backgroundImage: 'url(https://images.unsplash.com/photo-1531366936337-7c912a4589a7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80), linear-gradient(to bottom, transparent, rgba(0, 0, 0, 0.54))',
+                            backgroundSize: 'cover, cover',
+                            backgroundPosition: 'center',
+                            backgroundBlendMode: 'multiply',
+                            width: '100%',
+                            textAlign: 'center',
+                            paddingTop: 100,
+                            paddingBottom: 100,
+                            height: 200
+                        }}>
+                        </div>
+                    </Col>
+                    <Col xs={12} sm={12} md={6} lg={6}>
+                        <div>
+                            <p style={{fontSize:40, margin:0}}>LOREM IPSUM</p>
+                            <hr style={{width:'50%',textAlign:'left'}}/>
+                            <p style={{fontSize:20, marginTop:'0'}}>LOREM IPSUM</p>
+                            <p style={{}}>LOREM IPSUM</p>
+                            <h4>LOREM IPSUM</h4>
+                        </div>
+                    </Col>
+                </Row>
+            </Grid>
+        </Col>
+        
+        <Col style={{backgroundColor:"red"}}>
+            <Grid fluid>
+                <Row>
+                    <Col>
+                        <div style={{padding:" 20px  0px"}}>All rights reserved</div>
+                    </Col>
+                </Row>
+            </Grid>
+        </Col>
+
+        </React.Fragment>
+
+
     );
   }
 }
